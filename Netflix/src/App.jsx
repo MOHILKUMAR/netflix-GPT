2e0543084ec4c1d489d0db16adf17fa127f1d3d2
user.Mohil Kumar <user.mohil4280@gmail.com>
2025-05-24 15:22:31 +0530

    create & tailwind

---
+++ Netflix/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className='text-bold bg-red-600'>
+       Netflix
+      </div>
+    
+  )
+}
+
+export default App
